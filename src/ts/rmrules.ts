e23693f69f4e800f318cd6ab68955acff0a7181a
johanblumenberg <johan.blumenberg@gmail.com>
2017-11-19 14:38:37 +0100

    Adding support for removing overridden rules

--- src/ts/rmrules.ts
+++ src/ts/rmrules.ts
@@ -1,13 +1,40 @@
-import { Stylesheet, StyleRules, Rule, Node } from 'css';
+import { Stylesheet, StyleRules, Rule, Declaration, Comment, Node } from 'css';
 const CssSelectorParser = require('css-selector-parser').CssSelectorParser;
 
 const parser = new CssSelectorParser();
 parser.registerNestingOperators('>', '+', '~');
 
-export interface Options {
-    assumeSelectorsNotUsed?: string[];
+export enum Action {
+    IGNORE = '',
+    WARN = 'warn',
+    ERROR = 'error',
+    REMOVE = 'remove'
 }
 
+export interface Options {
+    assumeSelectorsNotUsed?: string[];
+    assumeSelectorsSet?: string[];
+
+    deadRules?: Action;
+    overriddenRules?: Action;
+}
+
+interface RuleIndex {
+    rule: any;
+    ruleIndex: number;
+    selectorIndex: number;
+};
+/*
+function log(fn: Function, name: string): Function {
+    return function () {
+        console.log(name);
+        console.log.apply(null, arguments);
+        let ret = fn.apply(this, arguments);
+        console.log(' - ', ret);
+        return ret;
+    };
+}
+*/
 function ruleUsesAnyOf(rule: any, anyOf: string[]): boolean {
     if (rule.classNames) {
         let anyOfClasses = anyOf.filter(sel => sel[0] === '.').map(sel => sel.substr(1));
@@ -20,11 +47,57 @@ function ruleUsesAnyOf(rule: any, anyOf: string[]): boolean {
     return false;
 }
 
-function selectorUsesAnyOf(selector: any, anyOf: string[]): boolean {
-    if (selector.type === 'ruleSet') {
-        return ruleUsesAnyOf(selector.rule, anyOf);
+function classesAreAlwaysOverriding(a: any[] | undefined, b: any[] | undefined, set: string[]): boolean {
+    let _a: { [className: string]: boolean } = {};
+    let _b: { [className: string]: boolean } = {};
+    let _s: { [className: string]: boolean } = {};
+    
+    if (a) {
+        a.forEach(className => _a[className] = true);
     }
-    return false;
+    if (b) {
+        b.forEach(className => _b[className] = true);
+    }
+    set.filter(className => className[0] === '.').map(className => className.substr(1)).forEach(className => _s[className] = true);
+
+    // Each of a must exist in b or set, or there are cases when a applies but not b
+    for (let className in _a) {
+        if (!_b[className] && !_s[className]) {
+            return false;
+        }
+    }
+
+    // Each of b must exist in a, or there are cases when b applies but not a
+    for (let className in _b) {
+        if (!_a[className]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function isAlwaysOverridingRule(a: any, b: any, set: string[]): boolean {
+    // TODO: check tag
+    // TODO: check id
+    // TODO: check nested operator
+
+    if (!a) {
+        return !b;
+    } else if (!b || !classesAreAlwaysOverriding(a.classNames, b.classNames, set)) {
+        return classesAreAlwaysOverriding(a.classNames, [], set) && isAlwaysOverridingRule(a.rule, b, set);
+    } else {
+        return isAlwaysOverridingRule(a.rule, b.rule, set);
+    }
+}
+//isAlwaysOverridingRule = log(isAlwaysOverridingRule, 'isAlwaysOverridingRule');
+
+function containsDeclaration(node: Node, declarations: Array<Declaration | Comment>) {
+    const decl = toDeclaration(node);
+    return decl && declarations.some(node => {
+        let decl2 = toDeclaration(node);
+        return !!decl2 && decl2.property === decl.property;
+    });
 }
 
 function toRule<T extends Node>(rule: T): Rule | undefined {
@@ -33,18 +106,95 @@ function toRule<T extends Node>(rule: T): Rule | undefined {
     }
 }
 
-export function rmrules(options: Options = {}): (style: StyleRules) => void {
-    let removeSelectors = options.assumeSelectorsNotUsed || [];
+function toDeclaration<T extends Node>(rule: T): Declaration | undefined {
+    if (rule.type === 'declaration') {
+        return <Declaration>rule;
+    }
+}
 
-    return (styles: StyleRules) => {
-        styles.rules = styles.rules.filter(rule => {
-            let _rule = toRule(rule);
-            if (_rule && _rule.selectors) {
-                _rule.selectors = _rule.selectors.filter((selector: string) => !selectorUsesAnyOf(parser.parse(selector), removeSelectors));
-                return _rule.selectors.length > 0;
-            } else {
-                return true;
+function removeDeadRules(rules: Node[], assumeSelectorsNotUsed: string[]) {
+    rules.forEach((rule, index) => {
+        let _rule = toRule(rule);
+        if (_rule && _rule.selectors) {
+            _rule.selectors = _rule.selectors.filter((selectorString: string) => {
+                let selector = parser.parse(selectorString);
+
+                return (selector.type !== 'ruleSet') || !ruleUsesAnyOf(selector.rule, assumeSelectorsNotUsed);
+            });
+        }
+    });
+}
+
+function collectRules(rules: Node[]) {
+    let result: RuleIndex[] = [];
+
+    rules.forEach((rule, ruleIndex) => {
+        let _rule = toRule(rule);
+        if (_rule && _rule.selectors) {
+            _rule.selectors.forEach((selectorString, selectorIndex) => {
+                let selector = parser.parse(selectorString);
+
+                if (selector.type === 'ruleSet') {
+                    result.push({ruleIndex, selectorIndex, rule: selector.rule});
+                }
+            });
+        }
+    });
+    return result;
+}
+
+function calculateOverridingRules(rules: RuleIndex[], assumeSelectorsSet: string[]) {
+    let result: { rule: RuleIndex, overrides: RuleIndex }[] = [];
+
+    rules.forEach((a, index) => {
+        rules.slice(index + 1).forEach(b => {
+            if (isAlwaysOverridingRule(b.rule, a.rule, assumeSelectorsSet)) {
+                // Also covers the case when the rules are identical
+                result.push({ rule: b, overrides: a });
+            } else if (isAlwaysOverridingRule(a.rule, b.rule, assumeSelectorsSet)) {
+                result.push({ rule: a, overrides: b });
             }
+        });
+    });
+
+    return result;
+}
+
+function overridesAllDeclarations(a: Declaration[], b: Declaration[]) {
+    return b.every(b_decoration => a.some(a_decoration => a_decoration.property === b_decoration.property));
+}
+
+function removeOverriddenDeclarations(rules: Node[], overrides: { rule: RuleIndex, overrides: RuleIndex }[]) {
+    overrides.forEach(({ rule, overrides }) => {
+        const a = toRule(rules[rule.ruleIndex]);
+        const b = toRule(rules[overrides.ruleIndex]);
+
+        if (a && a.declarations && b && b.declarations) {
+            if (b.selectors && b.selectors.length === 1) {
+                b.declarations = b.declarations.filter(node => !containsDeclaration(node, a.declarations || []));
+            } else if (b.selectors && b.selectors.length > 1) {
+                if (overridesAllDeclarations(a.declarations, b.declarations)) {
+                    b.selectors.splice(overrides.selectorIndex, 1);
+                }
+            }
+        }
+    });
+}
+
+export function rmrules(options: Options = {}): (style: StyleRules) => void {
+    let assumeSelectorsNotUsed = options.assumeSelectorsNotUsed || [];
+    let assumeSelectorsSet = options.assumeSelectorsSet || [];
+    
+    return (styles: StyleRules) => {
+        removeDeadRules(styles.rules, assumeSelectorsNotUsed);
+
+        let rules = collectRules(styles.rules);
+        let overrides = calculateOverridingRules(rules, assumeSelectorsSet);
+        removeOverriddenDeclarations(styles.rules, overrides);
+        
+        styles.rules = styles.rules.filter((rule, index) => {
+            let _rule = toRule(rule);
+            return !_rule || ((_rule.selectors && _rule.selectors.length) && (_rule.declarations && _rule.declarations.length));
         });
     }
 }
--- test/rmrules.spec.ts
+++ test/rmrules.spec.ts
@@ -1,144 +1,232 @@
 import { expect } from 'chai';
 import {} from 'jest';
-import { Options, rmrules } from '../src/ts/rmrules';
+import { Options, Action, rmrules } from '../src/ts/rmrules';
 import {} from 'node';
 const rework = require('rework');
 
 describe('rmrules', () => {
 
-    function rmrule(input: string, options?: Options) {
-        return rework(input).use(rmrules(options)).toString({ compress: true });
-    }
+    describe('remove dead rules', () => {
+        function rmrule(input: string, options: Options = { assumeSelectorsNotUsed: [ ".x" ], deadRules: Action.REMOVE }) {
+            return rework(input).use(rmrules(options)).toString({ compress: true });
+        }
+    
+        it('should remove simple use of x', () => {
+            let input = '.x { color: red; }';
+            let output = '';
+            expect(rmrule(input)).to.equal(output);
+        });
 
-    it('should remove simple use of x', () => {
-        let input = '.x { color: red; }';
-        let output = '';
-        expect(rmrule(input, { assumeSelectorsNotUsed: [ '.x' ]})).to.equal(output);
+        it('should keep other class names', () => {
+            let input = '.x { color: red; } .other { color: blue; }';
+            let output = '.other{color:blue;}';
+            expect(rmrule(input)).to.equal(output);
+        });
+
+        it('should remove rules where X is a parent selector', () => {
+            let input = '.x .abc { color: red; } .other { color: blue; }';
+            let output = '.other{color:blue;}';
+            expect(rmrule(input)).to.equal(output);
+        });
+
+        it('should remove rules where X has a parent selector', () => {
+            let input = '.abc .x { color: red; } .other { color: blue; }';
+            let output = '.other{color:blue;}';
+            expect(rmrule(input)).to.equal(output);
+        });
+
+        it('should remove rules where X and Y are classes on the same element, X is first', () => {
+            let input = '.abc .x.y { color: red; } .other { color: blue; }';
+            let output = '.other{color:blue;}';
+            expect(rmrule(input)).to.equal(output);
+        });
+
+        it('should remove rules where X and Y are classes on the same element, Y is first', () => {
+            let input = '.abc .y.x { color: red; } .other { color: blue; }';
+            let output = '.other{color:blue;}';
+            expect(rmrule(input)).to.equal(output);
+        });
+
+        it('should keep rules where X is part of a :not clause', () => {
+            let input = '.y:not(.x) { color: blue; }';
+            let output = '.y:not(.x){color:blue;}';
+            expect(rmrule(input)).to.equal(output);
+        });
+
+        it('should handle tag only selectors', () => {
+            let input = 'div { color: blue; }';
+            let output = 'div{color:blue;}';
+            expect(rmrule(input)).to.equal(output);
+        });
+
+        it('should handle id only selectors', () => {
+            let input = '#other { color: blue; }';
+            let output = '#other{color:blue;}';
+            expect(rmrule(input)).to.equal(output);
+        });
+
+        it('should not remove id with same name', () => {
+            let input = '#x { color: blue; }';
+            let output = '#x{color:blue;}';
+            expect(rmrule(input)).to.equal(output);
+        });
     });
 
-    it('should keep other class names', () => {
-        let input = '.x { color: red; } .other { color: blue; }';
-        let output = '.other{color:blue;}';
-        expect(rmrule(input, { assumeSelectorsNotUsed: [ '.x' ]})).to.equal(output);
+    describe('overridden rules', () => {
+        function rmrule(input: string, options: Options = { assumeSelectorsSet: [ ".x" ], overriddenRules: Action.REMOVE }) {
+            return rework(input).use(rmrules(options)).toString({ compress: true });
+        }
+    
+        it('should remove overridden rule with same selectors', () => {
+            let input = '.y { color: red; } .y { color: blue; }';
+            let output = '.y{color:blue;}';
+            expect(rmrule(input)).to.equal(output);            
+        });
+
+        it('should remove overridden rule with same selectors, classes in different order', () => {
+            let input = '.y.z { color: red; } .z.y { color: blue; }';
+            let output = '.z.y{color:blue;}';
+            expect(rmrule(input)).to.equal(output);            
+        });
+
+        it('should remove overridden rule, more specific with parent selector before', () => {
+            let input = '.x .y { color: red; } .y { color: blue; }';
+            let output = '.x .y{color:red;}';
+            expect(rmrule(input)).to.equal(output);            
+        });
+
+        it('should remove overridden rule, more specific with parent selector after', () => {
+            let input = '.y { color: red; } .x .y { color: blue; }';
+            let output = '.x .y{color:blue;}';
+            expect(rmrule(input)).to.equal(output);            
+        });
+
+        it('should remove overridden rule, more specific with child selector', () => {
+            let input = '.y .x { color: red; } .y { color: blue; }';
+            let output = '.y .x{color:red;}';
+            expect(rmrule(input)).to.equal(output);            
+        });
+
+        it('should remove overridden rule, more specific with extra class', () => {
+            let input = '.y.x { color: red; } .y { color: blue; }';
+            let output = '.y.x{color:red;}';
+            expect(rmrule(input)).to.equal(output);            
+        });
+
+        it('should remove overridden rule, more specific with extra class, classes in different order', () => {
+            let input = '.y.z.x { color: red; } .z.y { color: blue; }';
+            let output = '.y.z.x{color:red;}';
+            expect(rmrule(input)).to.equal(output);            
+        });
+
+        it('should not remove overridden rule if more specific but not always set', () => {
+            let input = '.y { color: red; } .y .z { color: blue; }';
+            let output = '.y{color:red;}.y .z{color:blue;}';
+            expect(rmrule(input)).to.equal(output);            
+        });
+
+        it('should only remove overridden declarations of a rule', () => {
+            let input = '.y { color: red; background: green; } .y { color: blue; }';
+            let output = '.y{background:green;}.y{color:blue;}';
+            expect(rmrule(input)).to.equal(output);            
+        });
+
+        it ('should only remove the affeted rule when several rules have the same declaration', () => {
+            let input = '.y, .z { color: red; } .y { color: blue; }';
+            let output = '.z{color:red;}.y{color:blue;}';
+            expect(rmrule(input)).to.equal(output);            
+        });
+
+        it ('should not remove a rule if all declarations are not overridden', () => {
+            let input = '.y, .z { color: red; background: green; } .y { color: blue; }';
+            let output = '.y,.z{color:red;background:green;}.y{color:blue;}';
+            expect(rmrule(input)).to.equal(output);            
+        });
+
+        /* TODO: leave for later, it's a tricky corner case
+        it ('should not remove a rule if all declarations are not overridden', () => {
+            let input = '.y, .z { color: red; background: green; } .y { color: blue; } .z { color: blue; }';
+            let output = '.y,.z{background:green;}.y{color:blue;},.z{color:blue;}';
+            expect(rmrule(input)).to.equal(output);
+        });
+        */
     });
 
-    it('should remove rules where X is a parent selector', () => {
-        let input = '.x .abc { color: red; } .other { color: blue; }';
-        let output = '.other{color:blue;}';
-        expect(rmrule(input, { assumeSelectorsNotUsed: [ '.x' ]})).to.equal(output);
-    });
+    describe('can parse', () => {
+        function rmrule(input: string) {
+            return rework(input).use(rmrules()).toString({ compress: true });
+        }
 
-    it('should remove rules where X has a parent selector', () => {
-        let input = '.abc .x { color: red; } .other { color: blue; }';
-        let output = '.other{color:blue;}';
-        expect(rmrule(input, { assumeSelectorsNotUsed: [ '.x' ]})).to.equal(output);
-    });
+        it('can parse @charset rules', () => {
+            let input = '@charset "utf-8";';
+            let output = '@charset "utf-8";';
+            expect(rmrule(input)).to.equal(output);
+        });
 
-    it('should remove rules where X and Y are classes on the same element, X is first', () => {
-        let input = '.abc .x.y { color: red; } .other { color: blue; }';
-        let output = '.other{color:blue;}';
-        expect(rmrule(input, { assumeSelectorsNotUsed: [ '.x' ]})).to.equal(output);
-    });
+        it('can parse @custom-media rules', () => {
+            let input = '@custom-media --breakpoint-sm (min-width: 40em);';
+            let output = '@custom-media --breakpoint-sm (min-width: 40em);';
+            expect(rmrule(input)).to.equal(output);
+        });
 
-    it('should remove rules where X and Y are classes on the same element, Y is first', () => {
-        let input = '.abc .y.x { color: red; } .other { color: blue; }';
-        let output = '.other{color:blue;}';
-        expect(rmrule(input, { assumeSelectorsNotUsed: [ '.x' ]})).to.equal(output);
-    });
+        it('can parse @document rules', () => {
+            let input = '@document url("https://www.example.com/") { .other { color: blue; } }';
+            let output = '@document url("https://www.example.com/"){.other{color:blue;}}';
+            expect(rmrule(input)).to.equal(output);
+        });
 
-    it('should keep rules where X is part of a :not clause', () => {
-        let input = '.y:not(.x) { color: blue; }';
-        let output = '.y:not(.x){color:blue;}';
-        expect(rmrule(input, { assumeSelectorsNotUsed: [ '.x' ]})).to.equal(output);
-    });
+        it('can parse @font-face rules', () => {
+            let input = '@font-face { font-family: font; src: url(font.tff); }';
+            let output = '@font-face{font-family:font;src:url(font.tff);}';
+            expect(rmrule(input)).to.equal(output);
+        });
 
-    it('should handle tag only selectors', () => {
-        let input = 'div { color: blue; }';
-        let output = 'div{color:blue;}';
-        expect(rmrule(input, { assumeSelectorsNotUsed: [ '.x' ]})).to.equal(output);
-    });
+        it('can parse @host rules', () => {
+            let input = '@host { .other { color: blue; } }';
+            let output = '@host{.other{color:blue;}}';
+            expect(rmrule(input)).to.equal(output);
+        });
 
-    it('should handle id only selectors', () => {
-        let input = '#other { color: blue; }';
-        let output = '#other{color:blue;}';
-        expect(rmrule(input, { assumeSelectorsNotUsed: [ '.x' ]})).to.equal(output);
-    });
+        it('can parse @import rules', () => {
+            let input = '@import url("other.css") print;';
+            let output = '@import url("other.css") print;';
+            expect(rmrule(input)).to.equal(output);
+        });
 
-    it('should not remove id with same name', () => {
-        let input = '#x { color: blue; }';
-        let output = '#x{color:blue;}';
-        expect(rmrule(input, { assumeSelectorsNotUsed: [ '.x' ]})).to.equal(output);
-    });
+        it('can parse @keyframes rules', () => {
+            let input = '@keyframes move { from { color: red; } to { color: blue; } }';
+            let output = '@keyframes move{from{color:red;}to{color:blue;}}';
+            expect(rmrule(input)).to.equal(output);
+        });
 
-    it('can parse @charset rules', () => {
-        let input = '@charset "utf-8";';
-        let output = '@charset "utf-8";';
-        expect(rmrule(input)).to.equal(output);
-    });
+        it('can parse @media rules', () => {
+            let input = '@media screen and (min-width: 100px) { .other { color: blue; } }';
+            let output = '@media screen and (min-width: 100px){.other{color:blue;}}';
+            expect(rmrule(input)).to.equal(output);
+        });
 
-    it('can parse @custom-media rules', () => {
-        let input = '@custom-media --breakpoint-sm (min-width: 40em);';
-        let output = '@custom-media --breakpoint-sm (min-width: 40em);';
-        expect(rmrule(input)).to.equal(output);
-    });
+        it('can parse @namespace rules', () => {
+            let input = '@namespace url(http://www.w3.org/1999/xhtml);';
+            let output = '@namespace url(http://www.w3.org/1999/xhtml);';
+            expect(rmrule(input)).to.equal(output);
+        });
 
-    it('can parse @document rules', () => {
-        let input = '@document url("https://www.example.com/") { .other { color: blue; } }';
-        let output = '@document url("https://www.example.com/"){.other{color:blue;}}';
-        expect(rmrule(input)).to.equal(output);
-    });
+        it('can parse @page rules', () => {
+            let input = '@page { margin: 1cm; }';
+            let output = '@page {margin:1cm;}';
+            expect(rmrule(input)).to.equal(output);
+        });
 
-    it('can parse @font-face rules', () => {
-        let input = '@font-face { font-family: font; src: url(font.tff); }';
-        let output = '@font-face{font-family:font;src:url(font.tff);}';
-        expect(rmrule(input)).to.equal(output);
-    });
+        it('can parse @supports rules', () => {
+            let input = '@supports (display: flex) { .other { display: flex; } }';
+            let output = '@supports (display: flex){.other{display:flex;}}';
+            expect(rmrule(input)).to.equal(output);
+        });
 
-    it('can parse @host rules', () => {
-        let input = '@host { .other { color: blue; } }';
-        let output = '@host{.other{color:blue;}}';
-        expect(rmrule(input)).to.equal(output);
-    });
-
-    it('can parse @import rules', () => {
-        let input = '@import url("other.css") print;';
-        let output = '@import url("other.css") print;';
-        expect(rmrule(input)).to.equal(output);
-    });
-
-    it('can parse @keyframes rules', () => {
-        let input = '@keyframes move { from { color: red; } to { color: blue; } }';
-        let output = '@keyframes move{from{color:red;}to{color:blue;}}';
-        expect(rmrule(input)).to.equal(output);
-    });
-
-    it('can parse @media rules', () => {
-        let input = '@media screen and (min-width: 100px) { .other { color: blue; } }';
-        let output = '@media screen and (min-width: 100px){.other{color:blue;}}';
-        expect(rmrule(input)).to.equal(output);
-    });
-
-    it('can parse @namespace rules', () => {
-        let input = '@namespace url(http://www.w3.org/1999/xhtml);';
-        let output = '@namespace url(http://www.w3.org/1999/xhtml);';
-        expect(rmrule(input)).to.equal(output);
-    });
-
-    it('can parse @page rules', () => {
-        let input = '@page { margin: 1cm; }';
-        let output = '@page {margin:1cm;}';
-        expect(rmrule(input)).to.equal(output);
-    });
-
-    it('can parse @supports rules', () => {
-        let input = '@supports (display: flex) { .other { display: flex; } }';
-        let output = '@supports (display: flex){.other{display:flex;}}';
-        expect(rmrule(input)).to.equal(output);
-    });
-
-    it('can parse empty document', () => {
-        let input = '';
-        let output = '';
-        expect(rmrule(input)).to.equal(output);
+        it('can parse empty document', () => {
+            let input = '';
+            let output = '';
+            expect(rmrule(input)).to.equal(output);
+        });
     });
 });
